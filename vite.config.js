0f0b474b82b5edadaba89f64adddbd1d088532ff
Mehak <mahakmimi01@gmail.com>
2026-08-28 09:38:33 +0500

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Ye line Vercel ko code todne se rokegi
+    minify: false, 
+  },
 })

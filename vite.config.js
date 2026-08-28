e927256baa92d278ce063e8e6d713ed87ad022f1
Mehak <mahakmimi01@gmail.com>
2026-08-28 09:54:24 +0500

    Downgraded React to 18

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Ye line Vercel ko code todne se rokegi
-    minify: false, 
-  },
 })

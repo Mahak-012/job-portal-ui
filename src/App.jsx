fca2fa208bf21548dd1e14ad28a88a49a1403a27
Mehak <mahakmimi01@gmail.com>
2026-08-29 21:19:20 +0500

    add job portal project to portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
-import{BrowserRouter,Routes,Route,useLocation}from"react-router-dom"
-import{useEffect}from"react"
-import{ThemeProvider}from"./context/ThemeContext"
-import Navbar    from"./components/Navbar"
-import Home      from"./pages/Home"
-import Jobs      from"./pages/Jobs"
-import JobDetail from"./pages/JobDetail"
-import Companies from"./pages/Companies"
-import Profile   from"./pages/Profile"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import { useEffect } from "react"
+import { ThemeProvider } from "./context/ThemeContext"
+import Navbar    from "./components/Navbar"
+import Home      from "./pages/Home"
+import Jobs      from "./pages/Jobs"
+import JobDetail from "./pages/JobDetail"
+import Companies from "./pages/Companies"
+import Profile   from "./pages/Profile"
 
-function ScrollTop(){
-  const{pathname}=useLocation()
-  useEffect(()=>window.scrollTo({top:0}),[pathname])
+function ScrollTop() {
+  const { pathname } = useLocation()
+  
+  // ✅ Sahi syntax: curly braces add kar diye hain
+  useEffect(() => {
+    window.scrollTo({ top: 0 })
+  }, [pathname])
+  
   return null
 }
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <ThemeProvider>
       <BrowserRouter>
-        <ScrollTop/>
-        <Navbar/>
+        <ScrollTop />
+        <Navbar />
         <Routes>
-          <Route path="/"          element={<Home/>}/>
-          <Route path="/jobs"      element={<Jobs/>}/>
-          <Route path="/jobs/:id"  element={<JobDetail/>}/>
-          <Route path="/companies" element={<Companies/>}/>
-          <Route path="/profile"   element={<Profile/>}/>
+          <Route path="/"         element={<Home />} />
+          <Route path="/jobs"     element={<Jobs />} />
+          <Route path="/jobs/:id" element={<JobDetail />} />
+          <Route path="/companies" element={<Companies />} />
+          <Route path="/profile"  element={<Profile />} />
+          <Route path="*"         element={<Home />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
